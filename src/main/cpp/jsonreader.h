3079fe12b5fe4b17ea3430016a23dfe88a027b70
shirleydu <shirley.du@makerbot.com>
2015-07-09 18:17:45 -0400

    support for receiving raw
    MW-5628
    http://jira.makerbot.net/browse/MW-5628

--- src/main/cpp/jsonreader.h
+++ src/main/cpp/jsonreader.h
@@ -20,8 +20,12 @@ class JsonReader : public JsonRpcStream {
   JSONRPC_API void feed(std::string const &);
   JSONRPC_API void feedeof(void);
 
+  JSONRPC_API void setRawHandler(
+      std::shared_ptr<std::function<void(char*, const size_t)> >,
+      const size_t);
+
  private:
-  enum State { S0, S1, S2, S3 };
+  enum State { S0, S1, S2, S3, S4 };
 
   void reset(void);
   void transition(char ch);
@@ -31,6 +35,11 @@ class JsonReader : public JsonRpcStream {
   State m_state;
   std::stack <char> m_stack;
   std::ostringstream m_buffer;
+
+  std::shared_ptr<std::function<void(char*, const size_t)> > m_rawHandler;
+  std::unique_ptr<char[]> m_rawBuffer;
+  size_t m_expectedLength = 0;
+  size_t m_rawBytesReceived = 0;
 
   // Disable copy constructor and assignment.
 
