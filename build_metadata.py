db6aca02ef5ad77df8150b28e789e4efb17afb90
Chris Moore <chris.moore@makerbot.com>
2019-12-19 14:14:42 -0500

    AUTOCOMMIT: Merging release 1.6 back to develop

--- build_metadata.py
+++ build_metadata.py
@@ -8,12 +8,12 @@ else:
     dependencies = [
         artifactory_utils.ArtifactSelector(
             project="Toolchain-Release",
-            revision="ReleaseMorepork1.5",
+            revision="develop",
             version='^4.6',
             stable_required=True),
         artifactory_utils.ArtifactSelector(
             project="json-cpp",
-            revision="ReleaseMorepork1.6",
+            revision="develop",
             stable_required=True)
     ]
     # TODO(chris): Use platform for morepork instead of this hack
@@ -21,6 +21,6 @@ else:
         dependencies.extend([
             artifactory_utils.ArtifactSelector(
                 project="morepork-json-cpp",
-                revision="ReleaseMorepork1.6",
+                revision="develop",
                 stable_required=True),
         ])
